3cc8649d606b39f9f39fa3d05247c25bc7b4ee8e
StudioEtrange <sboucault@gmail.com>
2019-10-06 04:02:12 +0200

    redirect stderr to null

--- lddtree.sh
+++ lddtree.sh
@@ -371,9 +371,9 @@ shift $(( $OPTIND - 1))
 ${SET_X} && set -x
 
 if [ -z "${BACKEND}" ]; then
-	if which scanelf >/dev/null; then
+	if which scanelf >/dev/null 2>&1; then
 		BACKEND=scanelf
-	elif which readelf >/dev/null; then
+	elif which readelf >/dev/null 2>&1; then
 		BACKEND=readelf
 	else
 		error "This tool needs either scanelf or readelf"
